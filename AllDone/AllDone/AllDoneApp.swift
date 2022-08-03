2747b7254bc1d971a2d6a5b8bed6e23e09033b8e
David E Bratton <debratton@Davids-MacBook-Pro.local>
2022-08-03 11:19:11 -0500

    Added XCode Project

---
+++ AllDone/AllDone/AllDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneApp.swift
+//  AllDone
+//
+//  Created by David E Bratton on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
